3ed2bac4593678e324cfefb562220963d7ed6ef1
rhegipatricio <rhegi.patricio@gmail.com>
2017-07-25 14:37:14 -0400

    began to add require

--- include/header.php
+++ include/header.php
@@ -1,10 +1,10 @@
 <header id="masthead">
     <div class="container">
-      <a href="index.html" id="logo">Rheginald Patricio</a>
+      <a href="index.php" id="logo">Rheginald Patricio</a>
       <nav>
-        <a href="index.html">About</a>
-        <a href="portfolio.html">Portfolio</a>
-        <a href="contact.html">Contact</a>
+        <a href="index.php">About</a>
+        <a href="portfolio.php">Portfolio</a>
+        <a href="contact.php">Contact</a>
       </nav>
     </div>
   </header>
