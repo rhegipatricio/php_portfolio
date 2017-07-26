7df7e3b72b4b90fe2468f37fac02c57cb7999dd1
rhegipatricio <rhegi.patricio@gmail.com>
2017-07-26 08:01:54 -0400

    moved header and sidebar

---
+++ include/header.php
@@ -0,0 +1,24 @@
+    <nav class="navbar navbar-default">
+      <div class="container-fluid navbar-custom">
+
+        
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#" id="logo">Rheginald Patricio</a>
+        </div>
+
+        
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right navbar-right-custom">
+            <li><a href="contact.php">Contact</a></li><span class="pipe"></span>
+            <li><a href="portfolio.php">Portfolio</a></li><span class="pipe"></span>
+            <li><a href="index.php">About</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
---
+++ include/sidebar.php
@@ -0,0 +1,11 @@
+            <section id="connect-with-me-area">
+              <div class="connect-with-me-content">
+                <h1 class="connect-with-me-header">Connect With Me</h1>
+                <hr />
+                <div id="images">
+                  <a href="https://github.com/rhegipatricio"><img src="images/github-128.png" alt="" class="connect-with-me-photo">
+                  <a href="https://www.linkedin.com/in/rheginaldpatricio/"><img src="images/linkedin-128.png" alt="" class="connect-with-me-photo">
+                  <a href="https://www.stackoverflow.com"><img src="images/stackoverflow-128.png" alt="" class="connect-with-me-photo">
+                </div>
+              </div>
+            </section>
