0543bf07f51ba239db493d7c1e0b5433b72d85d3
rhegipatricio <rhegi.patricio@gmail.com>
2017-07-26 06:55:12 -0400

    fixed index.php

--- contact.php
+++ contact.php
@@ -3,25 +3,18 @@
 
 
 <head>
-
-  <title>Rheginald Patricio Bootstrap Profile</title>
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <title>About | Rheginald Patricio</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
- <body>
+<body>
 
-<?php require '/include/header.php';?>
+<?php require 'include/header.php';?>
 
 
-    <!-- Start of content wrap -->
     <div class="wrap">
       <div class="container">
         <section class="row">
@@ -45,16 +38,15 @@
             </form>
           </div>
           <div class="col-md-4">
+          </div>
+        </section>
+      </div>
+    </div>
 
 
 <?php require '/include/sidebar.php';?>
 
 
-          </div>
-        </section>
-      </div>
-    </div>
-    <!-- End of content wrap -->
 
 <?php require '/include/footer.php';?>
 
--- index.php
+++ index.php
@@ -9,39 +9,10 @@
   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
-  <body>
-    <!-- Start of navbar -->
-    <nav class="navbar navbar-default">
-      <div class="container-fluid navbar-custom">
+<body>
 
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="row">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <div class="col-xs-12 phone-nav">
-              <a class="navbar-brand" href="#" id="logo">Your Name</a>
-            </div>
-          </div>
+<?php require 'include/header.php';?>
 
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right navbar-right-custom">
-              <li><a href="contact.php">Contact</a></li>
-              <li><a href="portfolio.php">Portfolio</a></li>
-              <li><a href="index.php">About</a></li>
-            </ul>
-          </div><!-- /.navbar-collapse -->
-        </div>
-      </div><!-- /.container-fluid -->
-    </nav>
-    <!-- End of navbar -->
-
-    <!-- Start of content wrap -->
     <div class="wrap">
       <div class="container">
         <section class="row">
@@ -50,9 +21,9 @@
               <h1 class="about-me-header">About Me</h1>
               <hr class="about-me-hr"/>
               <img src="assets/imgs/natebeach.jpeg" alt="" id="nate-image"/>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <br />
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p>Greetings, salutations, and welcome. This is Rheginald Patricio, future full stack web developer. I am currently in the process of learning and if you're reading this, then you are probably a part of my journey. For that, I thank you because this process of becoming a full stack developer seems very challenging. However, life is full of challenges</p>
+              <br>
+              <p>My life journey starts on September 13, 1982 in Camp Lejune, North Carolina. From there, my family and I migraged to Florida. I was raised in Oviedo, FL.</p>
             </article>
           </div>
           <div class="col-md-4">
@@ -69,22 +40,15 @@
                 </div>
               </div>
             </section>
-            <!-- End of the Connect With Me Area -->
-
           </div>
         </section>
       </div>
     </div>
-    <!-- End of content wrap -->
+<?php require '/include/sidebar.php';?>
 
-    <!-- Start of footer -->
-    <footer class="footer">
-      <div class="two-toned-footer-color"></div>
-      <p class="text-muted text-muted-footer text-center">
-        &copy; Copyright Rutgers Coding Bootcamp 2016
-      </p>
-    </footer>
-    <!-- End of footer -->
+
+
+<?php require '/include/footer.php';?>
 
     <!-- jQuery CDN -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
